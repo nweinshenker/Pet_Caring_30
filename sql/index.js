956b32eb96cbd943c48d579e4a65113268634831
nweinshenker <nweinshenker@wisc.edu>
2019-04-08 16:13:34 +0800

    Working on frontend

---
+++ sql/index.js
@@ -0,0 +1,12 @@
+const sql_list = {};
+
+sql_list.query = {
+    // Information Access
+    /* Access all caretakers with the same time availbility 
+    list_availbility 
+    count_caretakers
+    */
+
+}
+
+module.exports = sql_list
