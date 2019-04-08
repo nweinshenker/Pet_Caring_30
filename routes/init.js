cfb72251abf45593b9cb6a7e631ea995a447e3e2
nweinshenker <nweinshenker@wisc.edu>
2019-04-08 21:50:25 +0800

    cleanup to the project

--- routes/init.js
+++ routes/init.js
@@ -5,50 +5,39 @@ var uuid = require('uuid');
 // Postgre SQL Connection
 const { Pool } = require('pg');
 const pool = new Pool({
-	user: 'tarush',
+	user: 'nathanweinshenker',
 	host: 'localhost',
 	database: 'postgres',
 	password: 'postgres',
 	port: 5432,
 	// connectionString: process.env.DATABASE_URL,
-  //ssl: true
+	//ssl: true
 });
 
 const round = 10;
-const salt  = bcrypt.genSaltSync(round);
+const salt = bcrypt.genSaltSync(round);
 
 function initRouter(app) {
 	/* GET */
-	app.get('/'      , index );
-	// app.get('/search', search);
-	
-	/* PROTECTED GET */
-	// app.get('/dashboard', passport.authMiddleware(), dashboard);
-	// app.get('/games'    , passport.authMiddleware(), games    );
-	// app.get('/plays'    , passport.authMiddleware(), plays    );
-	
-	app.get('/register' , passport.antiMiddleware(), register );
-	app.get('/login' , passport.antiMiddleware(), getlogin );
+	app.get('/', index);
+
+	app.get('/register', passport.antiMiddleware(), register);
+	app.get('/login', passport.antiMiddleware(), getlogin);
 	app.get('/becomeOwner', passport.authMiddleware(), becomeOwner);
 	app.get('/becomeCaretaker', passport.authMiddleware(), becomeCaretaker);
-	app.get('/getlist',passport.authMiddleware(), getlist);
-	// app.get('/password' , passport.antiMiddleware(), retrieve );
-	
-	/* PROTECTED POST */
-	// app.post('/update_info', passport.authMiddleware(), update_info);
-	// app.post('/update_pass', passport.authMiddleware(), update_pass);
-	// app.post('/add_game'   , passport.authMiddleware(), add_game   );
-	// app.post('/add_play'   , passport.authMiddleware(), add_play   );
-	
+	app.get('/getlist', passport.authMiddleware(), getlist);
+	app.get('/caretaker', caretaker);
 
-	app.post('/reg_user'   , passport.antiMiddleware(), reg_user   );
+
+
+	app.post('/reg_user', passport.antiMiddleware(), reg_user);
 	app.post('/postlist', passport.authMiddleware(), postlist);
 	/* LOGIN */
 	app.post('/login', passport.authenticate('local', {
 		successRedirect: '/login',
 		failureRedirect: '/login'
 	}));
-	
+
 	/* LOGOUT */
 	app.get('/logout', passport.authMiddleware(), logout);
 }
@@ -62,45 +51,35 @@ function basic(req, res, page, other) {
 		// lastname : req.user.lastname,
 		// status   : req.user.status,
 	};
-	if(other) {
-		for(var fld in other) {
+	if (other) {
+		for (var fld in other) {
 			info[fld] = other[fld];
 		}
 	}
 	res.render(page, info);
 }
 
-// function query(req, fld) {
-// 	return req.query[fld] ? req.query[fld] : '';
-// }
-// function msg(req, fld, pass, fail) {
-// 	var info = query(req, fld);
-// 	return info ? (info=='pass' ? pass : fail) : '';
-
-// }
-
-function index(req,res,next){
+function index(req, res, next) {
 	res.render('index', { title: 'Express' });
 }
 
-function getlist(req,res,next){
-	res.render('list', { page: 'list' , title: 'Login' });
+function getlist(req, res, next) {
+	res.render('list', { page: 'list', title: 'Login' });
 
 }
 
-function postlist(req,res,next){
+function postlist(req, res, next) {
 	// console.log('hahahahaha');
 	console.log(req.user);
 	var sql_query = 'INSERT INTO list VALUES';
-	var id  = req.user.username;
+	var id = req.user.username;
 	// var name    = req.user.Name;
 	// var password = req.user.password;
-	sql_query = sql_query+"('"+id+"','"+req.body.day+"','"+req.body.price+"')";
-	pool.query(sql_query,function(err,result){
-		if(err)
+	sql_query = sql_query + "('" + id + "','" + req.body.day + "','" + req.body.price + "')";
+	pool.query(sql_query, function (err, result) {
+		if (err)
 			console.log(err);
-		else
-		{
+		else {
 			console.log(result);
 			res.redirect('/');
 			return;
@@ -109,68 +88,76 @@ function postlist(req,res,next){
 	// res.redirect('/');
 }
 
-function register(req,res,next){
-		res.render('register', { page: 'register' , auth: false, title: 'Owner Sign Up' ,
-		messages: {danger: req.flash('danger'), 
-		warning: req.flash('warning'), success: req.flash('success')}});
-		console.log(req.flash('warning'));
+function register(req, res, next) {
+	res.render('register', {
+		page: 'register', auth: false, title: 'Owner Sign Up',
+		messages: {
+			danger: req.flash('danger'),
+			warning: req.flash('warning'), success: req.flash('success')
+		}
+	});
+	console.log(req.flash('warning'));
 }
 
-function reg_user(req,res,next){
+function reg_user(req, res, next) {
 	// console.log(auth);
 	var sql_query = 'INSERT INTO users VALUES';
-	var id  = req.body.emailid;
-	var name    = req.body.Name;
+	var id = req.body.emailid;
+	var name = req.body.Name;
 	var password = req.body.password;
-	var check_query = 'SELECT userid FROM users WHERE userid=' + "'"+id+"'";
+	var check_query = 'SELECT userid FROM users WHERE userid=' + "'" + id + "'";
 	console.log(check_query);
 	var pwd = bcrypt.hashSync(req.body.password, salt);
 	console.log(pwd);
 	var insert_query = sql_query + "('" + id + "','" + name + "','" + pwd + "')";
-	pool.query(check_query, function(err, result) {
-	console.log(result.rows);
-	if(result.rows[0]){
-	req.flash('warning', "This email address is already registered.");
-	console.log("yoyoyooyooyyooyoyoyoy");
-	res.redirect('/register');
-	}
-	else{
-	pool.query(insert_query, function(err, result) {
-	if(err){console.log(err);}
-	else { 
-	// client.query('COMMIT')
-	console.log(result)
-	req.flash('success','User created.')
- 	res.redirect('/register');
-	return;
-	}
-	});
-	}
+	pool.query(check_query, function (err, result) {
+		console.log(result.rows);
+		if (result.rows[0]) {
+			req.flash('warning', "This email address is already registered.");
+			console.log("yoyoyooyooyyooyoyoyoy");
+			res.redirect('/register');
+		}
+		else {
+			pool.query(insert_query, function (err, result) {
+				if (err) { console.log(err); }
+				else {
+					// client.query('COMMIT')
+					console.log(result)
+					req.flash('success', 'User created.')
+					res.redirect('/register');
+					return;
+				}
+			});
+		}
 	});
 }
 
 
-function getlogin(req,res,next){
-	res.render('login', { page: 'login' , auth: false, title: 'Login' ,
-			messages: {danger: req.flash('danger'), 
-			warning: req.flash('warning'), success: req.flash('success')}});
+function getlogin(req, res, next) {
+	res.render('login', {
+		page: 'login', auth: false, title: 'Login',
+		messages: {
+			danger: req.flash('danger'),
+			warning: req.flash('warning'), success: req.flash('success')
+		}
+	});
 }
 
 
-function logout(req,res,next){
+function logout(req, res, next) {
 	req.session.destroy();
 	req.logout();
 	res.redirect('/');
 }
 
-function becomeOwner(req,res,next){
-	var insert_query = 'INSERT INTO owner VALUES'+"('"+req.user.username+"')";
+function becomeOwner(req, res, next) {
+	var insert_query = 'INSERT INTO owner VALUES' + "('" + req.user.username + "')";
 	// console.log(req.user);
 	console.log(req.user.username);
 	// insert_query
-	pool.query(insert_query, function(err,result){
-		if(err){console.log(err);}
-		else{
+	pool.query(insert_query, function (err, result) {
+		if (err) { console.log(err); }
+		else {
 			console.log(result)
 			res.redirect('/');
 			return;
@@ -179,24 +166,30 @@ function becomeOwner(req,res,next){
 	// res.redirect('/login');
 }
 
-function becomeCaretaker(req,res,next){
-	var insert_query = 'INSERT INTO caretaker VALUES'+"('"+req.user.username+"')";
+function becomeCaretaker(req, res, next) {
+	var insert_query = 'INSERT INTO caretaker VALUES' + "('" + req.user.username + "')";
 	// console.log(req.user);
 	console.log(req.user.username);
 	// insert_query
-	pool.query(insert_query, function(err,result){
-		if(err){
+	pool.query(insert_query, function (err, result) {
+		if (err) {
 			console.log(err);
 			res.redirect('/');
 			return;
 		}
-		else{
+		else {
 			console.log(result)
 			res.redirect('/');
 			return;
 		}
 	});
 	// res.redirect('/login');
+}
+
+function caretaker(req, res, next) {
+	res.render('/caretaker', {
+		title: 'Find Services'
+	});
 }
 
 
